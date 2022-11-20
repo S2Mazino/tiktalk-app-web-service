0d3786391fcb01fe5deeba9047cdf492163d86d7
nordine.chaumath@hotmail.com <nordine.chaumath@hotmail.com>
2022-11-19 19:56:50 -0800

    contacts get endpoint added

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ app.use('/chats', middleware.checkToken, require('./routes/chats.js'))
 
 app.use('/auth', middleware.checkToken, require('./routes/pushyregister.js'))
 
+app.use('/contacts', middleware.checkToken, require('./routes/contacts.js'))
+
 
 /*
  * Return HTML for the / end point. 
---
+++ routes/contacts.js
@@ -0,0 +1,66 @@
+//express is the framework we're going to use to handle requests
+const express = require('express')
+
+//Access the connection to Heroku Database
+const pool = require('../utilities/exports').pool
+
+const router = express.Router()
+
+const validation = require('../utilities').validation
+let isStringProvided = validation.isStringProvided
+
+
+router.post("/", (request, response, next) => {
+ 
+})
+
+/*
+* get all contacts associated with the token's member id
+*/
+router.get("/:memberId?", (request, response, next) => {
+    //validate member id exists
+    let query = 'SELECT * FROM Members WHERE memberid=$1'
+    //let values = [request.decoded.memberid]
+    let values = [request.params.memberId]
+
+    pool.query(query, values)
+        .then(result => {
+            if (result.rowCount == 0) {
+                response.status(404).send({
+                    message: "Member ID not found"
+                })
+            } else {
+                next()
+            }
+        }).catch(error => {
+            response.status(400).send({
+                message: "SQL Error",
+                error: error
+            })
+        })
+    }, (request, response) => {
+        //Retrieve the member's contact
+        let query = 'SELECT Members.FirstName, Members.LastName, Members.Nickname, Members.email, MemberID_B FROM Contacts INNER JOIN Members ON Contacts.MemberID_B = Members.MemberID where Contacts.MemberID_A = $1'
+        //let values = [request.decoded.memberid]
+        let values = [request.params.memberId]
+        pool.query(query, values)
+            .then(result => {
+                if (result.rowCount == 0) {
+                    response.status(404).send({
+                        message: "Contacts not found"
+                    })
+                } else {
+                    response.send({
+                        rowCount: result.rowCount,
+                        rows: result.rows
+                    })
+                }
+            }).catch(err => {
+                response.status(400).send({
+                    message: "SQL Error",
+                    error: err
+                })
+            })
+});
+
+module.exports = router
