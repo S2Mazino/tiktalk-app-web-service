094f25a0df332de081c91ec7a06b47dfe283d39b
nordine.chaumath@hotmail.com <nordine.chaumath@hotmail.com>
2022-11-29 01:06:50 -0800

    Added incoming and outgoing contact routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -10,11 +10,11 @@ const validation = require('../utilities').validation
 let isStringProvided = validation.isStringProvided
 
 
+//outgoing request send
 router.post("/:friendEmail?", (request, response, next) => {
     //validate member id exists
     let query = 'SELECT * FROM Members WHERE memberid=$1'
     let values = [request.decoded.memberid]
-    //let values = [request.params.memberId]
 
     pool.query(query, values)
         .then(result => {
@@ -43,7 +43,7 @@ router.post("/:friendEmail?", (request, response, next) => {
                     message: "friend email not found"
                 })
             } else {
-                request.params.memberid = result.rows[0].memberid
+                request.params.friendId = result.rows[0].memberid
                 next()
             }
         }).catch(error => {
@@ -53,10 +53,9 @@ router.post("/:friendEmail?", (request, response, next) => {
             })
         })
     },(request, response) => {
-        //Retrieve the member's contact
-        let query = 'INSERT INTO Contacts (primarykey, memberid_a, memberid_b, verified) VALUES (default, $1, $2, 1)'
-        let values = [request.decoded.memberid, request.params.memberid]
-        // let values = [request.params.memberId]
+        //Add a contact request
+        let query = 'INSERT INTO Contacts (primarykey, memberid_a, memberid_b, verified) VALUES (default, $1, $2, 0)'
+        let values = [request.decoded.memberid, request.params.friendId]
         pool.query(query, values)
             .then(result => {
                 //console.log(result.rows)
@@ -73,7 +72,7 @@ router.post("/:friendEmail?", (request, response, next) => {
 })
 
 /*
-* get all contacts associated with the token's member id
+* get all contacts friend associated with the token's member id
 */
 router.get("/", (request, response, next) => {
     //validate member id exists
@@ -98,7 +97,7 @@ router.get("/", (request, response, next) => {
         })
     }, (request, response) => {
         //Retrieve the member's contact
-        let query = 'SELECT Members.FirstName, Members.LastName, Members.Nickname, Members.email, MemberID_B FROM Contacts INNER JOIN Members ON Contacts.MemberID_B = Members.MemberID where Contacts.MemberID_A = $1'
+        let query = 'SELECT Members.FirstName, Members.LastName, Members.Nickname, Members.email, MemberID_B FROM Contacts INNER JOIN Members ON Contacts.MemberID_B = Members.MemberID where Contacts.MemberID_A = $1 AND Contacts.verified = 1'
         let values = [request.decoded.memberid]
         // let values = [request.params.memberId]
         pool.query(query, values)
@@ -142,7 +141,7 @@ router.delete("/:friendID?", (request, response, next) => {
     }
 }, (request, response, next) => {
     //memberid_a = person making the call, memberid_b = the person to be removed
-    let query = 'DELETE FROM Contacts WHERE memberid_a=$1 AND memberid_b=$2'
+    let query = 'DELETE FROM Contacts WHERE (memberid_a=$1 AND memberid_b=$2) OR (memberid_a=$2 AND memberid_b=$1)'
     let values = [request.decoded.memberid, request.params.friendID]
 
     pool.query(query, values)
@@ -152,7 +151,10 @@ router.delete("/:friendID?", (request, response, next) => {
                     message: "Contact does not exist"
                 })
             } else {
-                next()
+                //console.log(result.rows)
+                response.status(200).send({
+                    success: true
+                })
             }
         }).catch(error => {
             response.status(400).send({
@@ -160,30 +162,112 @@ router.delete("/:friendID?", (request, response, next) => {
                 error: error
             })
         })
-}, (request, response) => {
-    //Now deleteing from the friendID's contact as well
-    let query = 'DELETE FROM Contacts WHERE memberid_a=$2 AND memberid_b=$1'
-    let values = [request.decoded.memberid, request.params.friendID]
+});
+
+/*
+* get all outgoing contacts associated with the token's member id
+*/
+router.get("/search", (request, response, next) => {
+    //validate member id exists
+    let query = 'SELECT * FROM Members WHERE memberid=$1'
+    let values = [request.decoded.memberid]
+    //let values = [request.params.memberId]
+
     pool.query(query, values)
         .then(result => {
             if (result.rowCount == 0) {
                 response.status(404).send({
-                    message: "Contacts not found"
+                    message: "Member ID not found"
                 })
             } else {
-                //console.log(result.rows)
-                response.status(200).send({
-                    success: true
-                })
+                next()
             }
-        }).catch(err => {
-            //console.log(err)
+        }).catch(error => {
             response.status(400).send({
-                message: "SQL Error in contact delete",
-                error: err
+                message: "SQL Error in member validation",
+                error: error
             })
         })
-
+    }, (request, response) => {
+        //Retrieve the member's contact
+        let query = 'SELECT Members.FirstName, Members.LastName, Members.Nickname, Members.email, MemberID_B FROM Contacts INNER JOIN Members ON Contacts.MemberID_B = Members.MemberID where Contacts.MemberID_A = $1 AND Contacts.verified = 0'
+        let values = [request.decoded.memberid]
+        // let values = [request.params.memberId]
+        pool.query(query, values)
+            .then(result => {
+                if (result.rowCount == 0) {
+                    response.status(404).send({
+                        message: "No Contacts"
+                    })
+                } else {
+                    //console.log(result.rows)
+                    response.send({
+                        memberId: request.params.memberId,
+                        rowCount: result.rowCount,
+                        rows: result.rows
+                    })
+                }
+            }).catch(err => {
+                //console.log(err)
+                response.status(400).send({
+                    message: "SQL Error in contact retrieve",
+                    error: err
+                })
+            })
 });
+
+/*
+* get all incoming contacts associated with the token's member id
+*/
+router.get("/request", (request, response, next) => {
+    //validate member id exists
+    let query = 'SELECT * FROM Members WHERE memberid=$1'
+    let values = [request.decoded.memberid]
+    //let values = [request.params.memberId]
+
+    pool.query(query, values)
+        .then(result => {
+            if (result.rowCount == 0) {
+                response.status(404).send({
+                    message: "Member ID not found"
+                })
+            } else {
+                next()
+            }
+        }).catch(error => {
+            response.status(400).send({
+                message: "SQL Error in member validation",
+                error: error
+            })
+        })
+    }, (request, response) => {
+        //Retrieve the member's contact
+        let query = 'SELECT Members.FirstName, Members.LastName, Members.Nickname, Members.email, MemberID_B FROM Contacts INNER JOIN Members ON Contacts.MemberID_B = Members.MemberID where Contacts.MemberID_B = $1 AND Contacts.verified = 0'
+        let values = [request.decoded.memberid]
+        // let values = [request.params.memberId]
+        pool.query(query, values)
+            .then(result => {
+                if (result.rowCount == 0) {
+                    response.status(404).send({
+                        message: "No Contacts"
+                    })
+                } else {
+                    //console.log(result.rows)
+                    response.send({
+                        memberId: request.params.memberId,
+                        rowCount: result.rowCount,
+                        rows: result.rows
+                    })
+                }
+            }).catch(err => {
+                //console.log(err)
+                response.status(400).send({
+                    message: "SQL Error in contact retrieve",
+                    error: err
+                })
+            })
+});
+
+
 
 module.exports = router
