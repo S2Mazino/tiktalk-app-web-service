0ec53f8451e8ad431e730cef408c299880ff0519
nordine.chaumath@hotmail.com <nordine.chaumath@hotmail.com>
2022-11-29 01:07:45 -0800

    added more contacts to contacts.sql

--- SetupTestContact.sql
+++ SetupTestContact.sql
@@ -5,8 +5,8 @@ DELETE FROM Contacts;
 INSERT INTO
 	Contacts(PrimaryKey, MemberId_A, MemberId_B, Verified)
 VALUES
-	(DEFAULT, 73, (SELECT MemberId FROM Members WHERE Email='cfb3@uw.edu'), 1),
-	(DEFAULT, 75, (SELECT MemberId FROM Members WHERE Email='cfb3@uw.edu'), 1),
+	(DEFAULT, 73, (SELECT MemberId FROM Members WHERE Email='cfb3@uw.edu'), 0),
+	(DEFAULT, 75, (SELECT MemberId FROM Members WHERE Email='cfb3@uw.edu'), 0),
 	(DEFAULT, 73, (SELECT MemberId FROM Members WHERE Email='team3tiktalk2@gmail.com'), 1),
 	(DEFAULT, 75, (SELECT MemberId FROM Members WHERE Email='2twinparadox@gmail.com'), 1),
 	--adding dummy friends for team3tiktalk@gmail.com
@@ -16,5 +16,7 @@ VALUES
 	(DEFAULT, 73, 76, 1),
 	-- bronnyo@uw.edu and team3tiktalk@gmail.com are friends
 	(DEFAULT, 76, 89, 1),
-	(DEFAULT, 89, 76, 1)
+	(DEFAULT, 89, 76, 1),
+	--team3tiktalk@gmail.com sent friend request to 2twinparadox@gmail.com
+	(DEFAULT, 76, 75, 0)
 RETURNING *;
