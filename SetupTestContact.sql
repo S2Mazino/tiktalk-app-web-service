86cfd25d86242ac01d4746bcd5828e26576ccf6b
akannika <akannika@uw.edu>
2022-12-02 17:14:10 -0800

    Update the SetupContact to test

--- SetupTestContact.sql
+++ SetupTestContact.sql
@@ -3,8 +3,15 @@ DELETE FROM Contacts;
 
 --Add Knock & Charles as Friend
 INSERT INTO
-	Contacts(PrimaryKey, MemberId_A, MemberId_B, Verified)
-VALUES	
-	(DEFAULT, 12, (SELECT MemberId FROM Members WHERE Email='team3tiktalk2@gmail.com'), 1),
-	(DEFAULT, 12, (SELECT MemberId FROM Members WHERE Email='cfb3@uw.edu'), 1)
+    Contacts(PrimaryKey, MemberId_A, MemberId_B, Verified)
+VALUES    
+    (DEFAULT, 12, (SELECT MemberId FROM Members WHERE Email='team3tiktalk2@gmail.com'), 1),
+    (DEFAULT, 12, (SELECT MemberId FROM Members WHERE Email='cfb3@uw.edu'), 1),
+    --bronson and team3 friends
+    (DEFAULT, 14, 15, 1),
+    (DEFAULT, 15, 14, 1),
+    --team3 outgoing request
+    (DEFAULT, 15, 1, 0),
+    --team3 incoming request
+    (DEFAULT, 12, 15, 0)
 RETURNING *;
--- SetupTestMembers.sql
+++ SetupTestMembers.sql
@@ -9,7 +9,6 @@ VALUES
 	(2, 'Team3', 'Tiktalk', 'Team3', 'team3tiktalk@gmail.com', 1),
 	(3, 'Team3-2', 'Tiktalk', 'Team3-2', 'team3tiktalk2@gmail.com', 1),
 	(4, 'Test', 'Test', 'Test1', 'test@email.test', 1),
-	(5, 'bronnyo', 'bronnyo', 'bronnyo', 'bronnyo@uw.edu',1)
 RETURNING *;
 
 DELETE FROM Credentials;
