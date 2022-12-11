fa5754beea6407e20ace2f34e257db95349abc64
akannika <akannika@uw.edu>
2022-12-10 16:40:00 -0800

    Update the index.js

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ app.use('/auth', middleware.checkToken, require('./routes/pushyregister.js'))
 app.use("/weather", require("./routes/weather.js"));
 
 app.use('/contacts', middleware.checkToken, require('./routes/contacts.js'))
-app.use('/countNotification', middleware.checkToken, require('./routes/countNotification.js'))
 
 
 /*
