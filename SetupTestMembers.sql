795f606dbb8ca6921130ad7592c6bcb22bc8614a
nordine.chaumath@hotmail.com <nordine.chaumath@hotmail.com>
2023-01-06 23:17:47 -0800

    updated members sql script

--- SetupTestMembers.sql
+++ SetupTestMembers.sql
@@ -6,9 +6,8 @@ INSERT INTO
 	Members(MemberID, FirstName, LastName, Nickname, Email, Verification)
 VALUES
 	(1, 'Charles', 'B', 'Charles', 'cfb3@uw.edu', 1),
-	(2, 'Team3', 'Tiktalk', 'Team3', 'team3tiktalk@gmail.com', 1),
-	(3, 'Team3-2', 'Tiktalk', 'Team3-2', 'team3tiktalk2@gmail.com', 1),
-	(4, 'Test', 'Test', 'Test1', 'test@email.test', 1)
+	(2, 'Team3-2', 'Tiktalk', 'Team3-2', 'team3tiktalk2@gmail.com', 1),
+	(3, 'Test', 'Test', 'Test1', 'test@email.test', 1)
 RETURNING *;
 
 DELETE FROM Credentials;
