799489ebfcaffce4f80ea405b8cd8ae53dbbcb90
nordine.chaumath@hotmail.com <nordine.chaumath@hotmail.com>
2023-01-06 22:19:12 -0800

    updated contact and member sql setup

--- SetupTestContact.sql
+++ SetupTestContact.sql
@@ -5,13 +5,6 @@ DELETE FROM Contacts;
 INSERT INTO
     Contacts(PrimaryKey, MemberId_A, MemberId_B, Verified)
 VALUES    
-    (DEFAULT, 12, (SELECT MemberId FROM Members WHERE Email='team3tiktalk2@gmail.com'), 1),
-    (DEFAULT, 12, (SELECT MemberId FROM Members WHERE Email='cfb3@uw.edu'), 1),
-	--bronson and team3 friends
-	(DEFAULT, 14, 15, 1),
-	(DEFAULT, 15, 14, 1),
-	--team3 outgoing request
-	(DEFAULT, 15, 1, 0),
-	--team3 incoming request
-	(DEFAULT, 12, 15, 0)
+    (DEFAULT, (SELECT MemberId FROM Members WHERE Email='cfb3@uw.edu'), (SELECT MemberId FROM Members WHERE Email='team3tiktalk2@gmail.com'), 1),
+    (DEFAULT, (SELECT MemberId FROM Members WHERE Email='team3tiktalk2@gmail.com'), (SELECT MemberId FROM Members WHERE Email='cfb3@uw.edu'), 1)
 RETURNING *;
